0334622bdd272cf26d506380a5b9c39f822b5ea0
Dev Krishna Yadav <ydevkryadav09@gmail.com>
2026-04-20 11:06:56 +0000

    operators

--- learnbasics/conversionOperation.js
+++ learnbasics/conversionOperation.js
@@ -15,12 +15,51 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "Dev"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
  
 // 1 => true ; 0=> false
 // "" => false ; "Dev" => True
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof (stringNumber))
+//console.log(stringNumber)
+//console.log(typeof (stringNumber))
+
+
+
+// ***********Operations***********/
+let value = 4;
+let negValue = - value
+//console.log(negValue)
+
+//console.log(2+2);
+//console.log(2%3);
+//console.log(2^3);
+//console.log(3*3);
+//console.log(2-1);
+//console.log(2-3);
+
+let str1 = "hello"
+let str2 = "Dev"
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log("1" + 3);  // string and integer cannt be added
+// console.log(1 +"12");
+// console.log("1"+1+2) // output is 112 still in string
+// console.log(1 +3  +"2"); // now the output is 42 following the rules of conversion the documentation
+
+// we need to write beaitiful code make it preety
+// If the string is in the last then the first ones will be added arithmetically and then added as a string 
+// If the string is in first the all will be treated as a string
+
+// console.log(true)
+// console.log(+true); // outpput is true which we dont need
+
+let num1 , num2, num3
+num1 = num3 = num2 = 2 +2
+//console.log(num1)  // output is 4 but the code is not readability good
+
+let gameCounter = 100
+++gameCounter ;
+console.log(gameCounter);
