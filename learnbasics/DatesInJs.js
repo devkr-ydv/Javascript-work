9739d009a59e7c619d134b671700826c56511ebc
Dev Krishna Yadav <ydevkryadav09@gmail.com>
2026-04-26 05:55:04 +0000

    dates playing

---
+++ learnbasics/DatesInJs.js
@@ -0,0 +1,10 @@
+// Date object in js represent a single moment in a platform
+// calculated from Jan 1, 1970 . 
+// Suggest an API of Temporal , it is just an proposal
+// +++++++++++++=
+
+let myDate = new Date()
+console.log(myDate); // output 2026-04-26T05:52:27.783Z
+// we try to covert the date to string
+console.log(myDate.toString()); // output : Sun Apr 26 2026 05:53:37 GMT+0000 (Coordinated Universal Time)
+
