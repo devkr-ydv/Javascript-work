b3b9d9ced147e76a2505dcd7c0e9c2c8a7d5c034
Dev Krishna Yadav <ydevkryadav09@gmail.com>
2026-04-28 11:06:04 +0000

    array

--- basics_02/array.js
+++ basics_02/array.js
@@ -1,4 +1,15 @@
-// const firstArray = [1,4,6,3,5]
+ const firstArray = [1,4,6,3,5]
 // console.log(firstArray);
 // let secondArray = ["dev" , "B.Tech" ,"CSE" ]
 // console.log(secondArray)
+// Array is an object
+// Array are resizable in JS 
+console.log()
+// Array in JS make shallow copies(changes are made in original copies)
+const myHeros = ["spiderman","antman"]
+// We get prototype access in array 
+console.log(myHeros[0]);
+
+// Array Methods
+firstArray.push(5) //output [ 1, 4, 6, 3, 5, 5 ]
+console.log(firstArray);
