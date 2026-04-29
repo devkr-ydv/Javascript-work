61cb020bede1cba080c275d3aa5151b23a49b34f
Dev Krishna Yadav <ydevkryadav09@gmail.com>
2026-04-29 05:05:35 +0000

    Revise Daily

---
+++ Revision/methods.js
@@ -0,0 +1,32 @@
+/*Strings ,  Number , Math , Integer ,bigint, boolean , conversion, comparison, datatypes , Memory(Stack and Heap Memory)
+Array , Date&Time
+___________
+node.js
+declaration as compilation
+console.log(Math.floor(Math.random()*((max-min)+1))+min) // used in any games of 
+__________
+Number 
+  .valueof(), .toString(), .toFixed(), .toprecision(), .toLocaleString('en-IN'), 
+_________
+Math
+Math.random() , Math.floor() , Math.celi(), Math.sqrt() , Math.abs(), Math.round(),
+_____________
+String()
+let StringOne = "abcdefgl"
+
+.concat(), .toUpperCase(), .toLowerCase()," " , .charAt("2"), .indexOf("e"), .trim(), .replace('e','z') 
+
+____________
+Array[]
+indexOf(), .join() , .push(), .pop(), .unshift(), .shift(),
+ .includes(a) { to check whether a exist in array or not}
+  
+ .slice(a,b){make a new array from a to b-1 index and no change is made in original array in memory}
+ .splice(a,b) {make a new array from a to b included and change is also made in original array in memory}
+
+
+
+
+
+*/
+console.log("Revise Daily to Get Excellence")
