34d49e802f418dfdc649a585f2e252522d5a97f6
Dev Krishna Yadav <ydevkryadav09@gmail.com>
2026-05-01 10:51:15 +0000

    objects

---
+++ basics_02/02_objects.js
@@ -0,0 +1,64 @@
+// const instaUser = new Object() // this is a singleton object
+// console.log(instaUser);//{}
+
+const tiktokUser = {}  //this is also an object but not a singleton
+tiktokUser.id = "123azx"
+tiktokUser.name = "Max"
+tiktokUser.isLoggedIn = false
+
+//console.log(tiktokUser);
+
+// Now we will do objects inside object
+
+const regularUser = {
+    email: "zxee@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Dev",
+            lastname:"Yadav",
+        }
+    }
+
+}
+// console.log(regularUser.fullname);//output:{ userfullname: { firstname: 'Dev', lastname: 'Yadav' } }
+// console.log(regularUser.fullname.userfullname);//output:{ userfullname: { firstname: 'Dev', lastname: 'Yadav' } }
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+//const obj3 = {obj1,obj2}
+//const obj3 = Object.assign({},obj1,obj2)// merge the objects
+// All the objects will go in empty object as target
+
+const obj3 = { ...obj1,...obj2} // here we use the spread technique
+
+//console.log(obj3);
+// Object inside the array
+const users =[
+{
+    id:1,
+    email:"de@gmail.com",
+
+
+},
+{
+    id:2,
+    email:"de@gmail.com",
+
+
+},
+{
+    id:3,
+    email:"de@gmail.com",
+
+
+}
+]
+users[1].email
+console.log(tiktokUser);//{ id: '123azx', name: 'Max', isLoggedIn: false }
+
+console.log(Object.keys(tiktokUser));//[ 'id', 'name', 'isLoggedIn' ]
+// Datatype of this output is array , can now make loops and use it in various palces
+// Used while working with database
+console.log(Object.values(tiktokUser));//[ '123azx', 'Max', false ]
+console.log(Object.entries(tiktokUser));//[ [ 'id', '123azx' ], [ 'name', 'Max' ], [ 'isLoggedIn', false ] ]
+console.log(tiktokUser.hasOwnProperty('isLoggedIn'));//true
