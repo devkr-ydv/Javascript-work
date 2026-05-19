73238c8733ba600c595a89dd67d805d64ef69f22
Dev Krishna Yadav <ydevkryadav09@gmail.com>
2026-05-19 07:46:13 +0000

    dom and iterative methods revision

--- Revision/02_revision.js
+++ Revision/02_revision.js
@@ -87,6 +87,69 @@ accumulator , current value
  arrayName.reduce(  (acc,item) =>  {return acc + item.price} )  // item access the objects inside the array coming from database 
 
 
+-------------------------
+examples and explanation
+
+
+ // One more diffrerence between [for, while, do while] and that of [for of , for in] is that
+ we can call the parameter passed in[ for(const parameter of arrayName) and for(const num in objectName)]
+ in console.log(num) // we just need to give the paramter which has all the access to the values of array or object
+ 
+ 
+ whereas in [for, while, and do while] , in console.log(arrayName[parameter])  we have to write the arrayName[parameter]
+ 
+ const myPractice =[ "dev", "mohan", "rahul", "ritik"]
+ 
+ for(const name of myPractice){
+ 
+ if(name === "rahul"){
+ 
+    console.log(`The wanted name is ${myPractice[name]}`)
+ 
+      }
+     console.log(${myPractice[name]}) 
+ }
+ 
+ 
+ 
+ 
+ 
+ const myObject = {
+ 
+     name: "Dev",
+	 work: "Software Engineer ",
+	 workTime: 7
+ }
+ for(const data in myObject){
+ 
+ console.log( `The work time will be increased to ${data.workTime +2 }` )
+  }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ [in for, while, do while loop if the loop is on the index then just do the console.log(index)][but if there is loop on the array or object the we have to do console.log(arrayName[index])]
+ 
+ 
+ [in for of loop the parameter has the access of element of array ]
+ 
+ [in for in loop the parameter has the access to key of objects passed and to access the value we need to do console.log(objectName[parameter])                     ]
+ 
+ 
+ [in forEach(function(item){console.log(`The names are ${item.languageName}`)})            ]  // for object inside the array  (item has access to objects so we can use the (.) )
+    we dont need to do console.log( arrayName[item.languageName] )  
+ 
+ 
+ 
+ 
+ Now putting all these things inside the const yoyo = coding.forEach(  (item) => {}    )
+ 
+ Now if we call console.log(yoyo) then forEach doesnt return any values
+ 
+ but filter(), map(), reduce() returns the value of yoyo
 
 
 
---
+++ Revision/03_revisionDom.js
@@ -0,0 +1,27 @@
+/*
+
+To manipulate the thing in the web page we use the console part of the inspect in web browser
+
+also to access and manipulate things (objects)
+
+
+
+
+data object 
+
+
+document.getElementById('')
+document.getElementById('').getAttribute('')
+document.getElementById('').setAttribute('')
+
+
+
+title.innerText      = show the text that is not hidden from span
+title.textContent    = shows the text that is also in the span (full texts)
+
+
+
+
+
+
+*/
