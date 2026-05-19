02f3f95b15d5a496831d01afa4a22f502d73fcb2
Dev Krishna Yadav <ydevkryadav09@gmail.com>
2026-05-19 12:11:24 +0000

    html manipulation

--- Revision/03_revisionDom.js
+++ Revision/03_revisionDom.js
@@ -23,5 +23,15 @@ title.textContent    = shows the text that is also in the span (full texts)
 
 
 
+for
+ document.querySelectorAll('h1')
+myH1 = document.querySelectorAll('h1')
+
+myH1[0].style.color= 'green'
+
+
+we get the Node List and is similar to that of array but now all the features
+we need to mention the line where we want to change the things like background color
+
 
 */
