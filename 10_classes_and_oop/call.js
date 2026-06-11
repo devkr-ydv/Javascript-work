b873104f101634d9386c846c9cfa6be2f883cce9
Dev Krishna Yadav <ydevkryadav09@gmail.com>
2026-06-11 10:04:57 +0000

    call the function and put this keyword

---
+++ 10_classes_and_oop/call.js
@@ -0,0 +1,15 @@
+function SetUsername(username){
+     // complex DB calls
+    this.username = username
+    console.log("called");
+}
+
+function createUser(username, email, password){
+    
+    SetUsername.call(this,username)     // Explicitely call the function using call
+    this.email = email              // .call is used to hold the reference
+    this.password = password
+}
+
+const chai = new createUser("chai", "chai@fb.com", "123")
+console.log(chai);
